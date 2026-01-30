a90ef045b4ddfcd44ceaea49d9c1e25331d2886a
Malshii <kulasinghe1malshi@gmail.com>
2026-01-30 09:07:19 +0530

    updated phone number

--- components/Header.js
+++ components/Header.js
@@ -402,7 +402,7 @@ const Header = () => {
                 <svg className="w-5 h-5 mr-3 group-hover:scale-110 transition-transform flex-shrink-0 text-purple-600" fill="currentColor" viewBox="0 0 20 20">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
                 </svg>
-                <span className="text-sm font-medium">(+64) 21 246 3988</span>
+                <span className="text-sm font-medium">027 841 2236</span>
               </a>
             </div>
           </div>
